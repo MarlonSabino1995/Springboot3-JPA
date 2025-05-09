52b43391bcfc7627658e38098d9e9b72f1af9894
Marlon Sabino <sabinomarlon1@gmail.com>
2025-05-09 18:42:54 -0300

    Inserção e Deleção de User

--- src/main/java/com/projetandoemcasa/course/services/UserService.java
+++ src/main/java/com/projetandoemcasa/course/services/UserService.java
@@ -24,4 +24,12 @@ public class UserService {
 		
 		return user.get();
 	}
+	
+	public User insert(User obj) {
+		return repository.save(obj); 
+	}
+	
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
 } 
